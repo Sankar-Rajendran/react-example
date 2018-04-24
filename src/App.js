6b30a67991baaf9f5bb31be6b02347e0a20c9bf1
sankar.rajendran <sankar.rajendran@imaginea.com>
2018-04-24 18:03:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import './App.css';
+import Radium, { StyleRoot } from 'radium';
+import Person from './Person/Person';
+
+
+class App extends Component {
+
+  state = {
+    persons: [{
+      name: 'Sankar',
+      age: 28
+    },
+    {
+      name: 'Sesh',
+      age: 28
+    },
+    {
+      name: 'Vasanth',
+      age: 31
+    }],
+    showPersons: false
+  }
+
+
+  switchNameHandler = (newName) => {
+    //  DON'T DO THIS .THIS WON'T BE RECOGNIZED BY REACT this.state.persons[1].name = 'Kannan';
+
+    this.setState({
+      persons: [{
+        name: newName,
+        age: 28
+      },
+      {
+        name: 'Kannan',
+        age: 28
+      },
+      {
+        name: 'Vasanth',
+        age: 31
+      }]
+    })
+  }
+
+
+
+  nameChagnedHandler = (event, index) => {
+
+    const person = { ...this.state.persons[index] };
+
+    // const person = Object.assign({},this.state.persons[index]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[index] = person;
+
+    this.setState({
+      persons: persons
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
+
+
+  deletePersonHandler = (index) => {
+    const persons = this.state.persons.slice();
+    persons.splice(index, 1)
+    this.setState({
+      persons: persons
+    })
+  }
+
+  render() {
+    const classes = [];
+
+    const buttonStyle = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+              return (
+                <Person
+                  key={index}
+                  changed={(event) => this.nameChagnedHandler(event, index)}
+                  click={this.deletePersonHandler.bind(this, index)}
+                  name={person.name}
+                  age={person.age} />
+              )
+            })
+          }
+        </div>
+      )
+
+      buttonStyle.backgroundColor = 'red';
+
+      buttonStyle[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+
+
+
+
+      if (this.state.persons.length <= 2) {
+        classes.push('bold');
+      }
+      if (this.state.persons.length <= 1) {
+        classes.push('red');
+      }
+    }
+    return (
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi , First react app</h1>
+          <p className={classes.join(' ')}>This really works..!!!</p>
+          <button key="buttonSwitch" style={buttonStyle} onClick={this.switchNameHandler.bind(this, 'Sankar Rajendran')}>Switch Name</button>
+          <button style={buttonStyle} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {/* click={()=>this.switchNameHandler('First Person')} this is not efficient */}
+          {persons}
+        </div>
+      </StyleRoot>
+
+    );
+  }
+}
+
+export default Radium(App);
