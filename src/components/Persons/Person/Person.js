c38542da53eeaf0df514d289f85235247e7fbcd4
sankar.rajendran <sankar.rajendran@imaginea.com>
2018-05-07 14:59:05 +0530

    Use of Prop-Types to check types

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import Radium from 'radium';
-import classes from  './Person.css'
+import classes from './Person.css'
 
 import WithClass from '../../../hoc/WithClass_Component'
 
@@ -16,10 +17,16 @@ const person = (props) => {
         <WithClass classes={classes.Person}>
             <p onClick={props.click}>I am {props.name} and {props.age} years old..!!! </p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <input type="text" onChange={props.changed} value={props.name} />
         </WithClass>
     )
 }
 
+// Person.PropTypes = {
+//     click: PropTypes.func,
+//     name: PropTypes.string,
+//     age: PropTypes.number,
+//     changed: PropTypes.func
+// }
 
 export default person;
